3ead825c1f171c7748005b31a37d051cd005e824
DUNEFWZ <113181740+DUNEFWZ@users.noreply.github.com>
2025-06-24 20:38:11 +0700

    Update song_finder_app.py

--- song_finder_app.py
+++ song_finder_app.py
@@ -12,7 +12,7 @@ Application
 import streamlit as st
 import google.generativeai as genai
 import re
-from song_finder_module import explain_recommendation
+from song_finder_module import create_agent
 
 # Streamlit UI
 st.set_page_config(page_title="Mood-Based Song Recommender", page_icon="🎵")
